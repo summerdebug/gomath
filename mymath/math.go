b8eee7075bb2309265ff4613ee9a0e90d2177bab
summerdebug <summerdebug@gmail.com>
2025-03-06 16:13:53 +0200

    godoc added

---
+++ mymath/math.go
@@ -0,0 +1,9 @@
+// Package mymath provides basic math functions.
+package mymath
+
+// Add returns the [sum] of two integers.
+//
+// [sum]: https://www.mathsisfun.com/numbers/addition.html
+func Add(a, b int) int {
+	return a + b
+}
